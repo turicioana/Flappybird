c781489ffbb65e3c40a479b2e141afd7931d13ab
turicioana <turicioana@gmail.com>
2017-01-12 21:48:18 +0200

    Bird
    In continuare am introdus imaginea in format png pentru pasare, aceasta
    sare deplasandu-se pe axa (Oy atunci cand este apasata tasta Space cu un
    anumit numar de pixeli, cand tasta Space este eliberata, pasarea cade cu
    o viteza stabilita. Mai mult de atat am realizat si o functie care
    verifica coliziunea pasarii cu obstacolele, in caz de coliziune se va
    termina jocul, in cazul in care pasarea trece printre obstacole se vor
    castiga puncte.

--- Flappy bird/Source1.cpp
+++ Flappy bird/Source1.cpp
@@ -21,16 +21,21 @@ sf::Sprite floorImage;
 //sf::SoundBuffer get_point_buffer;
 //sf::SoundBuffer go_up_buffer;
 //sf::SoundBuffer die_buffer;
+
 sf::Texture birdTexture;
 sf::Sprite birdImage;
+
 //sf::Sound get_point;
 //sf::Sound go_up;
 //sf::Sound die;
-float y = 300;
-float x_bird = 30;
+float yBird = 300;
+float xBird = 30;
+
 float length = 50;
 float width = 50;
+
 int points = 0;
+
 float speed_bird = 3.5;
 sf::Texture pipe_downTexture;
 sf::Texture pipe_upTexture;
@@ -46,19 +51,19 @@ sf::Sprite pipe2_downImage;
 sf::Sprite pipe3_upImage;
 sf::Sprite pipe3_downImage;
 
+float xPipe1;
+float xPipe2 ;
+float xPipe3 ;
 
-float xPipe1 = 800;
-float xPipe2 = 1200;
-float xPipe3 = 1600;
-
-float yPipe1 = -275;
-float yPipe2 = -200;
-float yPipe3 = -250;
+float yPipe1 ;
+float yPipe2 ;
+float yPipe3 ;
 
 int free_space = 150;
 float pipe_len = 375;
 float pipe_width = 150;
 float speed_pipe= 4.5;
+void replay();
 
 int getRand(int A, int B)
 {
@@ -86,6 +91,9 @@ void loadFiles()
 
 	if (!logoTexture.loadFromFile("Images/flappy.png"))
 		throw std::runtime_error("Could not load flappy.png");
+
+	if (!birdTexture.loadFromFile("Images/bird.png"))
+		throw std::runtime_error("Could not load bird.png");
 }
 
 void setPipeAtPos(int x, int carePipe)
@@ -123,10 +131,10 @@ void Menu()
 	logoImage.setPosition(50, 10);
 
 	pipe1_downImage.setTexture(pipe_downTexture);
-	pipe1_downImage.setPosition(30, 400);
+	pipe1_downImage.setPosition(30, 410);
 
 	pipeMenu_Image.setTexture(pipe_downTexture);
-	pipeMenu_Image.setPosition(620, 330);
+	pipeMenu_Image.setPosition(620, 310);
 
 	//MENU
 	sf::Text menu("Menu", font, 135);
@@ -201,14 +209,71 @@ void Menu()
 	}
 }
 
+void resetBird()
+{
+	speed_bird = 3.5; 
+	yBird = 300;
+	xBird = 30; 
+	length = 50;
+	width = 50;
+	
+}
+
+void resetPipes()
+{
+	 xPipe1 = 800;
+	 xPipe2 = 1200;
+	 xPipe3 = 1600;
+	 speed_pipe = 4.5;
+	yPipe1 = getRand(-275, -75);
+	yPipe2 = getRand(-275, -75);
+	yPipe3 = getRand(-275, -75);
+}
+bool checkCollision(int x, int y)
+{
+	if(x <= xBird + width && x + pipe_width >= xBird + width)
+	{
+		if(yBird <= y + pipe_len)
+		return true;
+		if (yBird + length >= y + pipe_len + free_space)
+		return true;
+	}
+
+	if (yBird + length > 550)
+		return true;
+
+	return false;
+}
+
+void getPoints(int x, int y)
+{
+	if (x > xBird - pipe_width - 4.5 && x + pipe_width <= xBird && checkCollision(x, y) == false )
+	{
+		points++;
+	}
+	std::cout << points;
+}
+
 void Play()
 {
+	
+	points = 0;
+	resetPipes();
+	resetBird();
 	bkImage.setPosition(0, 0);
 	floorImage.setPosition(0, 550);
+	birdImage.setTexture(birdTexture);
+	birdImage.setPosition(xBird, yBird);
+
 	sf::Text back("Press Esc for going back to MENU", font, 20);
 	back.setStyle(sf::Text::Bold);
 	back.setFillColor(sf::Color(255, 0, 0));
 	back.setPosition(0, 573);
+
+	sf::Text button("Replay", font, 20);
+	button.setStyle(sf::Text::Bold);
+	button.setFillColor(sf::Color(255, 0, 0));
+	button.setPosition(740, 573);
 
 	pipe1_upImage.setTexture(pipe_upTexture);
 	pipe1_downImage.setTexture(pipe_downTexture);
@@ -237,7 +302,34 @@ void Play()
 				state = 0;
 			else if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::Escape)
 				state = 1;
+			else if (button.getGlobalBounds().contains(mouse) && sf::Mouse::isButtonPressed(sf::Mouse::Left))//Jocul o ia de la capat
+			{
+				replay();
+			}
+			else if (event.type == sf::Event::KeyPressed&&event.key.code == sf::Keyboard::Space)
+			{
+				if (speed_bird != 0)
+				{
+					yBird= yBird - 70;
+					//if (sounds) bird.go_up.play();
+				}
+			}
+			/*else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return) && speed_bird == 0)
+			{
+				state = 2;
+			}*/
 		}
+
+		if (checkCollision(xPipe1, yPipe1) || checkCollision(xPipe2, yPipe2) || checkCollision(xPipe3, yPipe3))
+		{
+			speed_bird = 0;
+			speed_pipe = 0;
+		}
+
+		getPoints(xPipe1, yPipe1);
+		getPoints(xPipe2, yPipe2);
+		getPoints(xPipe3, yPipe3);
+
 		if (xPipe1 <= -400)
 		{
 			xPipe1 = 800;
@@ -263,6 +355,9 @@ void Play()
 		setPipeAtPos(xPipe1, 1);
 		setPipeAtPos(xPipe2, 2);
 		setPipeAtPos(xPipe3, 3);
+		yBird = yBird + speed_bird;
+
+		birdImage.setPosition(xBird, yBird);
 
 		window.clear();
 		window.draw(bkImage);
@@ -275,10 +370,18 @@ void Play()
 		window.draw(pipe3_downImage);
 
 		window.draw(floorImage);
+		window.draw(birdImage);
 		window.draw(back);
+		window.draw(button);
 
 		window.display();
 	}
+}
+
+void replay()
+{
+	resetPipes();
+	Play();
 }
 
 void startGame()
